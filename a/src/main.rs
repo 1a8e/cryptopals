941205c29f9366bc06444af2c1ff2aaf4a3494a5
1a8e <5284980+1a8e@users.noreply.github.com>
2020-10-25 09:08:08 +0530

    split each challenge into a separate function

--- a/src/main.rs
+++ a/src/main.rs
@@ -3,11 +3,20 @@ extern crate base64;
 use std::u8;
 
 fn main() {
+    one();
+    two();
+}
+
+
+pub fn one() {
     let source_hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let destination_b64 = hex_to_base64(source_hex.to_string());
     println!("{}", source_hex);
     println!("{}", destination_b64);
 
+}
+
+pub fn two() {
     let first = "1c0111001f010100061a024b53535009181c";
     let second = "686974207468652062756c6c277320657965";
 
